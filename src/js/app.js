eeaddb82003f9b3343399a44b161430fbf3951e1
JoseAntonio-bc <joseanbarboza@gmail.com>
2025-02-13 16:26:06 +0000

    first commit

--- src/js/app.js
+++ src/js/app.js
@@ -35,27 +35,33 @@ function render(variables = {}) {
   if (variables.lastName === null) {
     variables.lastName = "lastName";
   }
-  if (variables.roll === null) {
-    variables.roll = "Occuption";
+  if (variables.role === null) {
+    variables.role = "role";
   }
   if (variables.city === null) {
     variables.city = "City";
   }
-
+  if (variables.country === null) {
+    variables.country = "Country";
+  }
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
           <h1>${variables.name} ${variables.lastName}</h1>
-          <h2>${variables.roll}</h2>
-          <h3>${variables.city}</h3>
+          <h2>${variables.role}</h2>
+          <h3>${variables.city}, ${variables.country}</h3>
           <ul class="${variables.socialMediaPosition}">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://twitter.com/${variables.twitter ||
+              "4geeksacademy"}" target="_blank"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${variables.github ||
+              "4geeksacademy"}" target="_blank"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${variables.linkedin ||
+              "school/4geeksacademy"}" target="_blank"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram ||
+              "4geeksacademy"}" target="_blank"><i class="fab fa-instagram"></i></a></li>
           </ul>
-        </div>
+            </div>
     `;
 }
 
